df75702e8803367488cc438673f96046f54d2b22
idlesseby <sebastian.moedritzer@gmail.com>
2024-02-11 21:21:24 +0100

    Add color transition shader

--- src/Examples/ColorTransition.jsx
+++ src/Examples/ColorTransition.jsx
@@ -4,30 +4,36 @@ import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 import { Center } from '@react-three/drei'
 import { Vector3 } from 'three'
+import { gsap } from "gsap";
 
 export default function Test() {
   const material = useRef()
-  let color = new Vector3(0.0,1.0,0.0)
+  let firstColor = new Vector3(0.0,0.0,1.0)
+  let secColor = new Vector3(1.0,0.0,1.0)
+  let progress = { value: 0 }
 
   useFrame(state => {
     const time = state.clock.getElapsedTime()
     material.current.uniforms.uTime.value = time
-    material.current.uniforms.uColor.value = color
+    material.current.uniforms.uProgress.value = progress.value
   })
 
   return <>
     <Center>
-      {/* mesh or points */}
-      <mesh onPointerOver={() => {color.set(1.0,0.0,1.0)}} onPointerOut={() => {color.set(0.0,1.0,0.0)}}> 
-        <sphereGeometry args={[1,32,16]}/>
+      <mesh 
+        onPointerEnter={() => gsap.to(progress, {value: 1.3, duration: 1})}
+        onPointerLeave={() => gsap.to(progress, {value: 0, duration: 1})}> 
+        <planeGeometry args={[1,1,10,10]}/>
         <shaderMaterial
           ref={material}
           vertexShader={vertex}
           fragmentShader={fragment}
-          wireframe
+          
           uniforms={{
               uTime: { value: 0 },
-              uColor: { value: 0 }
+              uProgress: { value: progress.value },
+              uFirstColor: { value: firstColor },
+              uSecColor: { value: secColor }
         }}
         />
       </mesh>
--- src/shaders/ColorTransition/fragment.glsl
+++ src/shaders/ColorTransition/fragment.glsl
@@ -1,6 +1,12 @@
+#define PI 3.1415926538
+
 varying vec2 vUv;
-uniform vec3 uColor;
+uniform vec3 uFirstColor;
+uniform vec3 uSecColor;
+uniform float uProgress;
 
 void main() {
-    gl_FragColor = vec4(uColor, 1.0);
+    float colorProgress = smoothstep(uProgress-0.3, uProgress, vUv.y);
+    vec3 color = mix(uFirstColor, uSecColor, colorProgress);
+    gl_FragColor = vec4(color, 1.0);
 }
